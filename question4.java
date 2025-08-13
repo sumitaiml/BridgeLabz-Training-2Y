a18e99824eb00edfa5a2d79f488cf63ee599a029
sumitaiml <vns.pratapns@gmail.com>
2025-08-13 19:37:49 +0530

    Q4 Uploaded

---
+++ question4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class question4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        int radius = sc.nextInt();
+        
+    
+        float area = 3.14f * radius * radius;
+        
+        
+        System.out.println("The area of the circle is " + area);
+    }
+}
