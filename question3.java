a01e3baea32b434860b2ed4b1bc2213f31d29bc7
sumitaiml <vns.pratapns@gmail.com>
2025-08-13 19:32:07 +0530

    Q3 Uploaded

---
+++ question3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class question3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float Celsius=sc.nextFloat();
+        float Fahrenheit = (Celsius * 9/5) + 32;
+        System.out.println("The teamperature in Fahrenheit "+Fahrenheit);
+    }
+}
