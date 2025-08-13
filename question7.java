6e34d1c914f2d9e3624667d7b8a687c6453378ff
sumitaiml <vns.pratapns@gmail.com>
2025-08-13 20:16:32 +0530

    Q7 Uploaded

---
+++ question7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class question7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int length=sc.nextInt();
+        int width=sc.nextInt();
+        int Perimeter = 2 * (length + width);
+        System.out.println("The perimeter of the recatngle is :"+Perimeter);
+    }
+}
