f0e83b5cd60f187d84059dee408c64dcc5f73c0c
sumitaiml <vns.pratapns@gmail.com>
2025-08-13 19:25:07 +0530

    Q2 Uploaded

---
+++ question2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class question2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int sum=n1+n2;
+        System.out.println("sum of the number one and  two is: "+sum);
+    }
+}
